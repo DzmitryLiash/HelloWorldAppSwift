3927d1ff214dc5da09d6b09706850545fa0aebc8
Dzmitry Liashchou <liashchou99@gmail.com>
2021-04-07 17:53:16 +0200

    First commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Дмитрий Лещёв on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
